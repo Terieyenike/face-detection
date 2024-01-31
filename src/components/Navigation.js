a57a417a6de76116c2af4b33515d4dfe2e083a45
Oteri Eyenike <teyenike1@gmail.com>
2024-01-31 13:38:31 +0100

    Feat: face recognition app and components setup

---
+++ src/components/Navigation.js
@@ -0,0 +1,9 @@
+const Navigation = () => {
+  return (
+    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+      <p className="f3 pa3 pointer link dim black underline">Sign out</p>
+    </nav>
+  )
+};
+
+export { Navigation };
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import { Navigation } from "./Navigation";
+import { Logo } from "./Logo";
+
+export { Navigation, Logo };
